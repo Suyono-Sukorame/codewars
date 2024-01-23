8e74b311d183a56e38bf097dd285e64ab893bb79
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-23 10:20:28 +0800

    humanReadable

---
+++ 2-6kyu-HumanReadableTime.js
@@ -0,0 +1,15 @@
+function humanReadable(seconds) {
+    if (seconds < 0 || seconds > 359999) {
+        return '00:00:00';
+    }
+
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedHours = hours.toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
