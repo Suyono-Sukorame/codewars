188aed499f07f5175e392901df569fc6fb938d34
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-03-18 15:35:36 +0800

    15-6kyu-The latest clock

---
+++ JavaScript/15-6kyu-The latest clock.js
@@ -0,0 +1,14 @@
+const latestClock = (a, b, c, d) => {
+    const validTimeReg = /(?:[01]\d|2[0-3]):[0-5]\d/;
+
+    const allCombos = [];
+    const makeCombos = (dgts, str = '') => {
+        !dgts.length && validTimeReg.test(str) && allCombos.push(str);
+        dgts.length === 2 && (str += ':');
+        dgts.forEach((dgt, idx) => makeCombos(dgts.filter((_, i) => i !== idx), str + dgt));
+    }
+
+    makeCombos([a, b, c, d]);
+
+    return allCombos.sort().at(-1) || '';
+};
